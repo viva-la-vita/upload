6c19bb003b825a99158fa3cfc90155cb10705c91
Songchen Tan <i@tansongchen.com>
2024-12-24 20:26:29 -0500

    Initial upload

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+interface Env {
+	ASSETS: R2Bucket;
+	AUTH_SECRET: string;
+}
+
+const headers = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': '*',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Max-Age': '86400',
+};
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env): Promise<Response> {
+		if (request.method === 'OPTIONS') return new Response(null, { status: 204, headers });
+		if (request.method !== 'POST') return new Response('Method Not Allowed', { status: 405, headers });
+		const data = await request.formData();
+		const files = data.getAll('file[]') as File[];
+		const succMap: Record<string, string> = {};
+		const errFiles: string[] = [];
+		const endpoint = 'https://assets.viva-la-vita.org/';
+		const futures = [];
+		for (const file of files) {
+			const timestamp = Math.round(new Date().getTime() / 1000);
+			const ext = /(?:\.([^.]+))?$/.exec(file.name)![1];
+			const key = `${timestamp}.${ext}`;
+			const future = env.ASSETS.put(key, file)
+				.then(() => {
+					succMap[key] = endpoint + key;
+				})
+				.catch(() => {
+					errFiles.push(file.name);
+				});
+			futures.push(future);
+		}
+		await Promise.all(futures);
+		const result = {
+			msg: '成功上传',
+			code: 0,
+			data: { errFiles, succMap },
+		};
+		return new Response(JSON.stringify(result), { status: 200, headers });
 	},
 } satisfies ExportedHandler<Env>;
